6ba4da87fc1300d0a5b2568f1b848f2a34d70c87
EdwardGoomba <edward@dock90.io>
2020-05-20 09:56:27 -0400

    Add babelrc file, fetch camera data

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,33 @@ const CameraViewer = styled.div`
   background: yellow;
 `
 
-const Home = () => (
-  <Container>
-    <Header />
-    <CameraViewer>
-      <p>Camera goes here</p>
-    </CameraViewer>
-  </Container>
-)
+const Home = ({ cameraData }) => {
+  console.log('Data: ', cameraData)
+
+  return (
+    <Container>
+      <Header />
+      <CameraViewer>
+        <p>Camera goes here</p>
+      </CameraViewer>
+    </Container>
+  )
+}
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get available cameras
+  // You can use any data fetching library
+  const res = await fetch('http://runningios.com/screamingbox/cameras.json')
+  const cameraData = await res.json()
+
+
+  // By returning { props: cameraData }, the Home component
+  // will receive `cameraData` as a prop at build time
+  return {
+    props: {
+      cameraData,
+    },
+  }
+}
 
 export default Home
